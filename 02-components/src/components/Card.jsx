7213de7380961ccb3e4a2d2fac1160a5e3650075
ShivamChaudhary <sc31122004@gmail.com>
2025-12-13 00:08:49 +0530

    components

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className="card">
+        <h1>Shivam Chaudhary</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, incidunt.</p>
+    </div> 
+  )
+}
+
+export default Card
