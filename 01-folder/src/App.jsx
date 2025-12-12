2726377256801f892302fd5002a0959841f9c098
ShivamChaudhary <sc31122004@gmail.com>
2025-12-12 15:28:26 +0530

    first commit

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,18 @@
+const App = () => {
+  return (
+    <>
+    <div id="Parent">
+      <h1 id="child1">Hello</h1>
+      <h2 id="child2">Hello Again</h2>
+    </div>
+
+    <div id="chacha">
+      <h1 id="chacha1">Chacha</h1>
+    </div>
+    
+    </>
+  )
+    
+}
+
+export default App
